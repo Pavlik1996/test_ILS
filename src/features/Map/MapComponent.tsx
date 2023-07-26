e9f42370e407d55d7e23f9f0b97836b180f53dd7
Pavlik1996 <miki2012miki@gmail.com>
2023-07-26 20:24:13 +0300

    v beta 0.5

--- src/features/Map/MapComponent.tsx
+++ src/features/Map/MapComponent.tsx
@@ -13,20 +13,8 @@ export const MapComponent = () => {
 	const centerRoute: LatLngExpression = [59.84660399, 30.29496392]
 	const [points, setPoints] = useState<LatLngExpression[]>([])
 
-
 	const routes = useSelector(selectRoutes)
 
-	const dispatch = useAppDispatch()
-
-	const arg: GetRouteType = {
-		startingPoint: { lat: 59.83567701, lng: 30.38064206 },
-		intermediatePoint: { lat: 59.84660399, lng: 30.29496392 },
-		endPoint: { lat: 59.82761295, lng: 30.41705607 }
-	}
-
-	useEffect(() => {
-		dispatch(mapThunks.fetchRoutes(arg))
-	}, [])
 
 	useEffect(() => {
 		if (routes) {
@@ -35,21 +23,7 @@ export const MapComponent = () => {
 		}
 	}, [routes])
 
-	const polyline_1: LatLngExpression[] = [
-		[59.84660399, 30.29496392],
-		[59.82934196, 30.42423701],
-		[59.83567701, 30.38064206]
-	]
-	const polyline_2: LatLngExpression[] = [
-		[59.82934196, 30.42423701],
-		[59.82761295, 30.41705607],
-		[59.84660399, 30.29496392]
-	]
-	const polyline_3: LatLngExpression[] = [
-		[59.83567701, 30.38064206],
-		[59.84660399, 30.29496392],
-		[59.82761295, 30.41705607]
-	]
+
 
 
 	return (
@@ -59,9 +33,8 @@ export const MapComponent = () => {
 				url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 			/>
 			<Polyline color={'red'} positions={points}></Polyline>
-			<Marker position={polyline_3[0]} />
-			<Marker position={polyline_3[1]} />
-			<Marker position={polyline_3[2]} />
+			{/*<Marker position={polyline_3[0]} />*/}
+
 		</MapContainer>
 	)
 }
--- src/features/Table/TableRoute.tsx
+++ src/features/Table/TableRoute.tsx
@@ -1,50 +1,92 @@
 import { Table } from 'antd'
 import s from './TableRoute.module.scss'
-import { useEffect } from 'react'
 import { useAppDispatch } from '../../common/hooks/useAppDispatch'
-import { GetRouteType, tableApi } from './table.api'
+import { GetRouteType } from './table.api'
+import { mapThunks } from '../routeSlice/routeSlice.ts'
+
+type RecordType = {
+	key: string
+	place_1: number[]
+	place_2: number[]
+	place_3: number[]
+	route: string
+}
 
 export const TableRout = () => {
-	const dataSource = [
+
+	const dispatch = useAppDispatch()
+
+	const dataSource: RecordType[] = [
 		{
 			key: '1',
-			name: 'Mike',
-			age: 32,
-			address: '10 Downing Street'
+			route: 'Маршрут#1',
+			place_1: [59.84660399, 30.29496392],
+			place_2: [59.82934196, 30.42423701],
+			place_3: [59.83567701, 30.38064206]
 		},
 		{
 			key: '2',
-			name: 'John',
-			age: 42,
-			address: '10 Downing Street'
+			route: 'Маршрут#1',
+			place_1: [59.82934196, 30.42423701],
+			place_2: [59.82761295, 30.41705607],
+			place_3: [59.84660399, 30.29496392]
+		},
+		{
+			key: '3',
+			route: 'Маршрут#1',
+			place_1: [59.83567701, 30.38064206],
+			place_2: [59.84660399, 30.29496392],
+			place_3: [59.82761295, 30.41705607]
 		}
 	]
 
 	const columns = [
 		{
-			title: 'Name',
-			dataIndex: 'name',
-			key: 'name'
+			title: 'Маршрут',
+			dataIndex: 'route',
+			key: 'route'
 		},
 		{
-			title: 'Age',
-			dataIndex: 'age',
-			key: 'age'
+			title: 'Точка 1',
+			dataIndex: 'place_1',
+			key: 'place_1'
 		},
 		{
-			title: 'Address',
-			dataIndex: 'address',
-			key: 'address'
+			title: 'Точка 2',
+			dataIndex: 'place_2',
+			key: 'place_2'
+		},
+		{
+			title: 'Точка 3',
+			dataIndex: 'place_3',
+			key: 'place_3'
 		}
 	]
+
+	const onClickHander = (record: RecordType) => {
+		const data: GetRouteType = {
+			startingPoint: {
+				lat: record.place_1[0],
+				lng: record.place_1[1]
+			},
+			intermediatePoint: {
+				lat: record.place_2[0],
+				lng: record.place_2[1]
+			},
+			endPoint: {
+				lat: record.place_3[0],
+				lng: record.place_3[1]
+			}
+		}
+		dispatch(mapThunks.fetchRoutes(data))
+	}
+
 
 	return (
 		<Table
 			onRow={record => {
 				return {
-					onClick: e => {
-						console.log(record)
-					}
+					onClick: e => onClickHander(record)
 				}
 			}}
 			columns={columns}
