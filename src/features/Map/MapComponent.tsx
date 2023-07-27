07fdc067dee681a0193a2ebbf03bc8dc5de20de9
Pavlik1996 <miki2012miki@gmail.com>
2023-07-27 19:28:54 +0300

    add icon for Marker

--- src/features/Map/MapComponent.tsx
+++ src/features/Map/MapComponent.tsx
@@ -1,9 +1,11 @@
-import { LatLngExpression } from 'leaflet'
+import { LatLngExpression, Icon } from 'leaflet'
 import { MapContainer, Polyline, TileLayer, Marker } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import { selectCurrentRoutes, selectRoutes, selectShowMarker } from '../routeSlice/routeSelector'
 import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import mapIconGreen from '../../common/images/free-icon-placeholder-6964820.png'
+import mapIconRed from '../../common/images/map-icon-red.png'
 
 export const MapComponent = () => {
 	const zoom: number = 13
@@ -14,12 +16,22 @@ export const MapComponent = () => {
 	const currentRoutes = useSelector(selectCurrentRoutes)
 	const showMarker = useSelector(selectShowMarker)
 
+	const dischargingIconGreen = new Icon({
+		iconUrl: mapIconGreen,
+		iconSize: [43, 43]
+	})
+
+	const dischargingIconRed = new Icon({
+		iconUrl: mapIconRed,
+		iconSize: [46, 46]
+	})
+
 	useEffect(() => {
 		if (routes) {
 			// @ts-ignore
 			setPoints(routes.map(el => [el[1], el[0]]))
 		}
-	}, [routes, centerRoute])
+	}, [routes])
 
 	return (
 		<MapContainer center={centerRoute} zoom={zoom} scrollWheelZoom={true}>
@@ -27,12 +39,24 @@ export const MapComponent = () => {
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 			/>
-			<Polyline color={'red'} positions={points}></Polyline>
+			<Polyline color={'green'} positions={points}></Polyline>
 			{showMarker && (
 				<>
-					<Marker position={currentRoutes.startingPoint} />
-					<Marker position={currentRoutes.intermediatePoint} />
-					<Marker position={currentRoutes.endPoint} />
+					<Marker
+						position={currentRoutes.startingPoint}
+						title={'Загрузка'}
+						icon={dischargingIconRed}
+					/>
+					<Marker
+						position={currentRoutes.intermediatePoint}
+						title={'Разгрузка 1'}
+						icon={dischargingIconGreen}
+					/>
+					<Marker
+						position={currentRoutes.endPoint}
+						title={'Разгрузка 2'}
+						icon={dischargingIconGreen}
+					/>
 				</>
 			)}
 		</MapContainer>
